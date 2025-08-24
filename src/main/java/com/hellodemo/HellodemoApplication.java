c51355fe92c6b6da14532df03c34da4b71ac8800
azhar98 <azharuddin.mondal98@gmail.com>
2025-08-24 21:26:30 +0530

    removed messages from main file

--- src/main/java/com/hellodemo/HellodemoApplication.java
+++ src/main/java/com/hellodemo/HellodemoApplication.java
@@ -8,7 +8,7 @@ public class HellodemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellodemoApplication.class, args);
-		System.out.println("Hello World !");
+		System.out.println("Hello World main!");
 	}
 
 }
