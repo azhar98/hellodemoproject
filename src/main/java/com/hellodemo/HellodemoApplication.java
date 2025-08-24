6a6b69d6087f18628d4b54c230a3a7313bc69f02
azhar98 <65587399+azhar98@users.noreply.github.com>
2025-08-24 20:40:36 +0530

    Update HellodemoApplication.java
    changes the message in main file

--- src/main/java/com/hellodemo/HellodemoApplication.java
+++ src/main/java/com/hellodemo/HellodemoApplication.java
@@ -9,7 +9,7 @@ public class HellodemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HellodemoApplication.class, args);
 
-		System.out.println("hello");
+		System.out.println("Hello World");
 	}
 
 }
